7d557f39667a40b5b9720e971939900694ada56d
William Pursell <william.r.pursell@gmail.com>
2020-05-20 14:42:25 +0000

    Add very simple test of find node
    This does not do much, but getting from zero tests to 1 naive
    test is a big step.  Test suite can grow from here.

---
+++ find-node-test.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <err.h>
+
+static void
+expect(const char * name, struct node *returned, struct node *expected)
+{
+	if( expected != returned ) {
+		err(EXIT_FAILURE, "Test %s failed!!", name);
+	}
+}
+
+int
+main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	struct node a = { .id = 1 };
+	struct node b = { .id = 2 };
+	struct node p = { .id = 0, .c = { &a, &b } };
+	expect("NULL", find_node(NULL, 0), NULL);
+	expect("find 1", find_node(&p, 2), &b);
+	return 0;
+}
