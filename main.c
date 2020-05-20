c8e9421a9c87719eb70b1873f570c906c8587611
William Pursell <william.r.pursell@gmail.com>
2020-05-20 16:17:46 +0000

    Add more tests

--- main.c
+++ main.c
@@ -978,7 +978,34 @@ main2(int argc, char **argv)
 	}
 	focus(view_root);
 	draw(view_root);
-	run();
+	if( argv[1] && strcmp(argv[1], "test" ) ) {
+		run();
+	} else {
+		struct node *c[2];
+		create(root, NULL);
+		c[0] = find_node(root, 1);
+		c[1] = find_node(root, 2);
+		assert(c[1] == focused);
+		assert(c[0] == root->c[0]);
+		assert(c[1] == root->c[1]);
+		mov(c[1], (const char *[]) { "j", NULL });
+		assert(c[1] == focused);
+		mov(c[1], (const char *[]) { "k", NULL });
+		assert(c[0] == focused);
+		reorient(c[0], NULL);
+		assert(c[0] == focused);
+		mov(c[0], (const char *[]) { "l", NULL });
+		assert(c[1] == focused);
+		mov(c[1], (const char *[]) { "h", NULL });
+		assert(c[0] == focused);
+		redrawroot(c[0], NULL);
+		equalize(c[0], NULL);
+		cmd_count = 2;
+		swap(c[0],NULL);
+		assert(c[0] == focused);
+		assert(c[1] == root->c[0]);
+		assert(c[0] == root->c[1]);
+	}
 	endwin();
 	return EXIT_SUCCESS;
 }
