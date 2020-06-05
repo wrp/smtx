b545dbb9d3f6685ca7533c2cfd48b4ddb71bd563
William Pursell <william.r.pursell@gmail.com>
2020-06-05 22:01:07 +0000

    Trivial reformat

--- smtx-test.c
+++ smtx-test.c
@@ -1,4 +1,5 @@
 #include "smtx.h"
+#include <err.h>
 #include <sys/wait.h>
 
 const char *args[10];
@@ -6,32 +7,32 @@ const char *args[10];
 static void
 test1() {
 	int fd;
+
 	switch( forkpty(&fd, NULL, NULL, NULL) ) {
 	case -1:
 		err(1, "forkpty");
 	case 0:
 		smtx_main(1, (char *const[]) { "smtx-test", NULL });
 		exit(0);
-	default: {
-		char *cmds[] = {
-			"tput cud 2; tput cuu 2; tput cuf 1",
-			"tput ed; tput bel",
-			"tput hpa 5; tput ri",
-			"tput cub 1; tput dch 1; tput ack",
-			"tput civis; tput cvvis; tput ack",
-			"tabs -5",
-			"exit",
-			NULL
-		};
-		int status;
-		for( char **cmd = cmds; *cmd; cmd++ ) {
-			write(fd, *cmd, strlen(*cmd));
-			write(fd, "\r", 1);
-		}
-		wait(&status);
-		assert( WIFEXITED(status) && WEXITSTATUS(status) == 0 );
-		}
 	}
+	char *cmds[] = {
+		"echo err >&2;",
+		"tput cud 2; tput cuu 2; tput cuf 1",
+		"tput ed; tput bel",
+		"tput hpa 5; tput ri",
+		"tput cub 1; tput dch 1; tput ack",
+		"tput civis; tput cvvis; tput ack",
+		"tabs -5",
+		"exit",
+		NULL
+	};
+	int status;
+	for( char **cmd = cmds; *cmd; cmd++ ) {
+		write(fd, *cmd, strlen(*cmd));
+		write(fd, "\r", 1);
+	}
+	wait(&status);
+	assert( WIFEXITED(status) && WEXITSTATUS(status) == 0 );
 }
 
 int
