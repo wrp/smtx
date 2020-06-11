0bc6c357e92c007c80c4541b2bac958c88908d4f
William Pursell <william.r.pursell@gmail.com>
2020-06-11 16:31:21 +0000

    Run all tests in a child

--- smtx-test.c
+++ smtx-test.c
@@ -2,30 +2,16 @@
 #include <err.h>
 #include <sys/wait.h>
 
-static void
-endwin_wrap(void)
-{
-	(void)endwin;
-}
-
 static int
-test_cuu() {
-	init(24, 80);
+test_cuu(int fd)
+{
+	(void) fd;
 	return 0;
 }
 
 static int
-test1() {
-	int fd;
-	int status;
-
-	switch( forkpty(&fd, NULL, NULL, NULL) ) {
-	case -1:
-		err(1, "forkpty");
-	case 0:
-		status = smtx_main(1, (char *const[]) { "smtx-test", NULL });
-		exit(status);
-	}
+test1(int fd)
+{
 	char *cmds[] = {
 		"echo err >&2;",
 		"tput cud 2; tput cuu 2; tput cuf 1",
@@ -41,34 +27,54 @@ test1() {
 		write(fd, *cmd, strlen(*cmd));
 		write(fd, "\r", 1);
 	}
-	wait(&status);
-	return ! ( WIFEXITED(status) && WEXITSTATUS(status) == 0 );
+	return 0;
 }
 
-typedef int test(void);
-#define F(x) { .name = #x, .f = (x) }
+typedef int test(int);
+#define F(x, y) { .name = #x, .f = (x), .main = y }
 int
 main(int argc, char **argv)
 {
-	char *defaults[] = { "test1", "test_cuu", NULL };
-	struct { char *name; test *f; } tab[] = {
-		F(test1),
-		F(test_cuu),
-		{ NULL, NULL }
+	int rv = EXIT_SUCCESS;
+	char *const args[] = { "smtx-test", "test_cuu", NULL };
+	char *defaults[] = { "test1", NULL };
+	struct { char *name; test *f; int main; } tab[] = {
+		F(test1, 1),
+		F(test_cuu, 0),
+		{ NULL, NULL, 0 }
 	}, *v;
-	if( atexit(endwin_wrap)) {
-		err(EXIT_FAILURE, "atexit");
-	}
 	for( argv = argc < 2 ? defaults : argv + 1; *argv; argv++ ) {
 		for( v = tab; v->name && strcmp(v->name, *argv); v++ )
 			;
 		if( v->f ) {
-			if( v->f()) {
-				errx(EXIT_FAILURE, "test %s FAILED", *argv);
+			int fd;
+			int status;
+
+			switch( forkpty(&fd, NULL, NULL, NULL) ) {
+			case -1:
+				fprintf(stderr, "forkpty: %s", strerror(errno));
+				rv = EXIT_FAILURE;
+				break;
+			case 0:
+				if( v->main ) {
+					exit(smtx_main(1, args));
+				} else {
+					exit(v->f(fd));
+				}
+			default:
+				if( v->main ) {
+					v->f(fd);
+				}
+				wait(&status);
+			}
+			if( ! WIFEXITED(status) || WEXITSTATUS(status) != 0 ) {
+				rv = EXIT_FAILURE;
+				fprintf(stderr, "test %s FAILED", *argv);
 			}
 		} else {
-			errx(EXIT_FAILURE, "unknown function: %s", *argv);
+			fprintf(stderr, "unknown function: %s", *argv);
+			rv = EXIT_FAILURE;
 		}
 	}
-	return EXIT_SUCCESS;
+	return rv;
 }
