7abc8babcdba60929fbd38407679f4252d02ec7f
William Pursell <william.r.pursell@gmail.com>
2020-06-11 11:18:53 +0000

    Stub in test_cuu

--- smtx-test.c
+++ smtx-test.c
@@ -2,7 +2,17 @@
 #include <err.h>
 #include <sys/wait.h>
 
-const char *args[10];
+static void
+endwin_wrap(void)
+{
+	(void)endwin;
+}
+
+static int
+test_cuu() {
+	init();
+	return 0;
+}
 
 static int
 test1() {
@@ -32,7 +42,7 @@ test1() {
 		write(fd, "\r", 1);
 	}
 	wait(&status);
-	return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+	return ! ( WIFEXITED(status) && WEXITSTATUS(status) == 0 );
 }
 
 typedef int test(void);
@@ -40,16 +50,20 @@ typedef int test(void);
 int
 main(int argc, char **argv)
 {
-	char *defaults[] = { "test1", NULL };
+	char *defaults[] = { "test1", "test_cuu", NULL };
 	struct { char *name; test *f; } tab[] = {
 		F(test1),
+		F(test_cuu),
 		{ NULL, NULL }
 	}, *v;
+	if( atexit(endwin_wrap)) {
+		err(EXIT_FAILURE, "atexit");
+	}
 	for( argv = argc < 2 ? defaults : argv + 1; *argv; argv++ ) {
 		for( v = tab; v->name && strcmp(v->name, *argv); v++ )
 			;
 		if( v->f ) {
-			if( !v->f()) {
+			if( v->f()) {
 				errx(EXIT_FAILURE, "test %s FAILED", *argv);
 			}
 		} else {
