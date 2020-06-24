04c843011e226d9002f3881f10b61e924a410560
William Pursell <william.r.pursell@gmail.com>
2020-06-24 23:38:53 +0000

    Trivial reformat

--- handler.c
+++ handler.c
@@ -261,16 +261,18 @@ case dsr: { /* DSR - Device Status Report */
 	safewrite(n->pt, buf, strlen(buf));
 	} break;
 
-case idl: { /* IL or DL - Insert/Delete Line */
-    /* we don't use insdelln here because it inserts above and not below,
-     * and has a few other edge cases... */
-    int p1 = MIN(P1(0), (my - 1) - y);
-    wgetscrreg(win, &otop, &obot);
-    wsetscrreg(win, py, obot);
-    wscrl(win, w == L'L'? -p1 : p1);
-    wsetscrreg(win, otop, obot);
-    wmove(win, py, 0);
-	} break;
+case idl: /* Insert/Delete Line */
+	/* we don't use insdelln here because it inserts above and not below,
+	* and has a few other edge cases... */
+	{
+		int p1 = MIN(P1(0), my - 1 - y);
+		wgetscrreg(win, &otop, &obot);
+		wsetscrreg(win, py, obot);
+		wscrl(win, w == L'L' ? -p1 : p1);
+		wsetscrreg(win, otop, obot);
+		wmove(win, py, 0);
+	}
+	break;
 
 case csr: { /* CSR - Change Scrolling Region */
     if (wsetscrreg(win, tos + P1(0) - 1, tos + PD(1, my) - 1) == OK)
