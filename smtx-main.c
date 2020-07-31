31aa74223dab835b8d528857c332167bd56c5710
William Pursell <william.r.pursell@gmail.com>
2020-07-31 12:25:32 +0000

    Always set title and fix background
    This is preparation to change rs_row to always be the size of the largest canvas.

--- smtx-main.c
+++ smtx-main.c
@@ -408,18 +408,18 @@ reshape(struct canvas *n, int y, int x, int h, int w)
 			set_title(n);
 			if( p->fd >= 0 && changed ) {
 				reshape_window(n);
-				set_title(n);
-				if( n->extent.x > p->ws.ws_col ) {
-					int d = n->extent.x - p->ws.ws_col;
-					resize_pad(&n->bkg, n->extent.y, d);
-					wbkgd(n->bkg, ACS_CKBOARD);
-					pnoutrefresh(n->bkg, 0, 0,
-						n->origin.y,
-						n->origin.x + p->ws.ws_col,
-						n->origin.y + n->extent.y - 1,
-						n->origin.x + n->extent.x - 1
-					);
-				}
+			}
+			set_title(n);
+			if( n->extent.x > p->ws.ws_col ) {
+				int d = n->extent.x - p->ws.ws_col;
+				resize_pad(&n->bkg, n->extent.y, d);
+				wbkgd(n->bkg, ACS_CKBOARD);
+				pnoutrefresh(n->bkg, 0, 0,
+					n->origin.y,
+					n->origin.x + p->ws.ws_col,
+					n->origin.y + n->extent.y - 1,
+					n->origin.x + n->extent.x - 1
+				);
 			}
 			scrollbottom(n);
 		} else if( w1 && h1 > 1 ) {
