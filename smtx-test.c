4c0242e763d0985be4f36768d464c9d21c2b4e6a
William Pursell <william.r.pursell@gmail.com>
2020-08-02 12:08:48 +0000

    Remove intrusive tests from smtx-test.c

--- smtx-test.c
+++ smtx-test.c
@@ -9,23 +9,6 @@ int child_pipe[2];
 static unsigned describe_layout(char *, size_t, const struct canvas *,
 	int, int);
 
-static unsigned
-describe_row(char *desc, size_t siz, WINDOW *w, int row)
-{
-	int oy, ox;
-	unsigned x, mx;
-	getmaxyx(w, x, mx); /* x unused; reset below */
-	mx = mx < siz ? mx : siz - 1;
-	getyx(w, oy, ox);
-	for( x = 0; x < mx; x++ ) {
-		chtype c = mvwinch(w, row, x);
-		desc[x] = c & A_CHARTEXT;
-	}
-	desc[x] = '\0';
-	wmove(w, oy, ox);
-	return x;
-}
-
 static void
 send_cmd(int fd, const char *fmt, ...)
 {
@@ -38,24 +21,6 @@ send_cmd(int fd, const char *fmt, ...)
 	assert( n < sizeof cmd );
 	rewrite(fd, cmd, n);
 	rewrite(fd, "\r", 1);
-}
-
-static void
-vexpect_row(int row, int col, WINDOW *w, const char *fmt, va_list ap)
-{
-	char actual[1024];
-	char expect[1024];
-	const char *a = actual + col, *b = expect;
-	describe_row(actual, sizeof actual, w, row);
-	vsnprintf(expect, sizeof expect, fmt, ap);
-	while( *a && *a++ == *b ) {
-		b += 1;
-	}
-	if( *a || *b ) {
-		warnx("\nrow %d Expected \"%s\", but got \"%s\"\n",
-			row, expect, actual + col);
-		rv = EXIT_FAILURE;
-	}
 }
 
 static void
@@ -91,42 +56,12 @@ struct test_canvas {
 };
 
 static void
-expect_row(int row, struct test_canvas *T, const char *expect, ...)
-{
-	/* TODO: need to fix this to handle a canvas with children */
-	va_list ap;
-	va_start(ap, expect);
-	vexpect_row(row + T->c->offset.y, T->c->offset.x, T->w, expect, ap);
-	va_end(ap);
-}
-
-static void
 expect_layout(const struct canvas *c, const char *expect, ...)
 {
 	va_list ap;
 	va_start(ap, expect);
 	vexpect_layout(c, expect, ap);
 	va_end(ap);
-}
-
-static int
-test_description(int fd)
-{
-	(void)fd;
-	int row = 1000;
-	struct canvas *r = init();
-	expect_layout(r, "*23x80@0,0(%d,0)", row);
-	create(r, "c");
-	expect_layout(r, "*11x80@0,0(%d,0); 11x80@12,0(%d,0)", row, row);
-	mov(r, "j");
-	expect_layout(r, "11x80@0,0(%d,0); *11x80@12,0(%d,0)", row, row);
-	create(r->c[0], "C");
-	expect_layout(r, "11x80@0,0(%d,0); *11x40@12,0(%d,0); "
-		"11x39@12,41(%d,0)", row, row, row);
-	mov(r->c[0], "l");
-	expect_layout(r, "11x80@0,0(%d,0); 11x40@12,0(%d,0); "
-		"*11x39@12,41(%d,0)", row, row, row);
-	return rv;
 }
 
 static void
@@ -191,23 +126,6 @@ new_test_canvas(int rows, int cols, const char *ps1)
 }
 
 static int
-test_nel(int fd)
-{
-	(void) fd;
-	setenv("TERM", "smtx", 1);
-	const char *cmd = "tput cud 3; printf foo; tput nel; echo blah";
-	struct test_canvas *T = new_test_canvas(10, 80, NULL);
-	check_cmd(T, cmd, NULL);
-	expect_row(5, T, "%-80s", "foo");
-	expect_row(6, T, "%-80s", "blah");
-	cmd = "printf foobar; tput cub 3; tput el; echo blah";
-	check_cmd(T, cmd, NULL);
-	expect_row(7, T, "%-80s", "fooblah");
-	check_cmd(T, cmd, NULL);
-	return rv;
-}
-
-static int
 test_navigate(int fd)
 {
 	ssize_t s;
@@ -246,239 +164,6 @@ test_navigate(int fd)
 	write(fd, buf, strlen(buf));
 	return status;
 }
-
-static int
-test_csr(int fd)
-{
-	(void) fd;
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	char *cmd = "tput csr 6 12";
-	check_cmd(T, cmd, NULL);
-	check_cmd(T, "yes | nl | sed 25q", NULL);
-	for(int i = 2; i < 6; i++ ) {
-		expect_row(i, T, "     %d  %-72s", i, "y");
-	}
-	for(int i = 6; i < 12; i++ ) {
-		expect_row(i, T, "    %d  %-72s", i + 14, "y");
-	}
-	expect_row(12, T, "%-80s", T->ps1);
-	return rv;
-}
-
-static int
-test_cols(int fd)
-{
-	/* Ensure that tput correctly identifies the width */
-	(void) fd;
-	setenv("TERM", "smtx", 1);
-	struct test_canvas *T = new_test_canvas(10, 97, NULL);
-	check_cmd(T, "tput cols", NULL);
-	expect_row(2, T, "%-97s", "97");
-	return rv;
-}
-
-static int
-test_scrollback(int fd)
-{
-	(void) fd;
-	char cmd[80] = "yes | nl | sed 50q";
-	const char *string = "This is a relatively long string, dragon!";
-	snprintf(cmd, sizeof cmd, "yes %s | nl | sed 50q", string);
-	S.history = 20;
-	struct test_canvas *T = new_test_canvas(10, 80, NULL);
-	check_cmd(T, cmd, NULL);
-	expect_row(0, T, "%6d  %-72s", 43, string);
-	expect_row(7, T, "%6d  %-72s", 50, string);
-	expect_row(8, T, "%-80s", T->ps1, string);
-	cmd_count = 8;
-	scrolln(T->c, "-");
-	expect_row(0, T, "%6d  %-72s", 35, string);
-	expect_row(7, T, "%6d  %-72s", 42, string);
-	expect_row(8, T, "%6d  %-72s", 43, string);
-
-	cmd_count = 3;
-	scrolln(T->c, "+");
-	expect_row(0, T, "%6d  %-72s", 38, string);
-	expect_row(7, T, "%6d  %-72s", 45, string);
-	expect_row(8, T, "%6d  %-72s", 46, string);
-
-	cmd_count = 2;
-	/* make the window larger so scrollh is not a no-op */
-	T->c->extent.x = 60;
-	scrollh(T->c, ">");
-	expect_row(0, T, "%4d  %-72s", 38, string);
-	expect_row(7, T, "%4d  %-72s", 45, string);
-	expect_row(8, T, "%4d  %-72s", 46, string);
-
-	cmd_count = 1;
-	scrollh(T->c, "<");
-	expect_row(0, T, "%5d  %-72s", 38, string);
-	expect_row(7, T, "%5d  %-72s", 45, string);
-	expect_row(8, T, "%5d  %-72s", 46, string);
-
-	cmd_count = -1;
-	scrollh(T->c, ">");
-	expect_row(0, T, "%-60s", string + 80 - 60 - 8);
-	expect_row(7, T, "%-60s", string + 80 - 60 - 8);
-	expect_row(8, T, "%-60s", string + 80 - 60 - 8);
-	return rv;
-}
-
-static int
-test_ich(int fd)
-{
-	(void) fd;
-	const char *cmd = "printf abcdefg; tput cub 3; tput ich 5; echo";
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	check_cmd(T, cmd, NULL);
-	expect_row(2, T, "abcd     efg%-68s", "");
-	expect_row(3, T, "%-80s", T->ps1);
-	cmd = "yes | nl | sed 6q; tput cuu 3; tput il 3; tput cud 6";
-	check_cmd(T, cmd, "*23x80@0,0(1014,6)");
-	expect_row(3, T, "%s%-74s", T->ps1, cmd);
-	for( int i=1; i < 4; i++ ) {
-		expect_row(3 + i, T, "     %d  y%71s", i, "");
-	}
-	for( int i=4; i < 7; i++ ) {
-		expect_row(3 + i, T, "%80s", "");
-	}
-	for( int i=7; i < 10; i++ ) {
-		expect_row(3 + i, T, "     %d  y%71s", i - 3, "");
-	}
-	expect_row(13, T, "%-80s", T->ps1);
-	cmd = "yes | nl | sed 6q; tput cuu 5; tput dl 4; tput cud 1";
-	check_cmd(T, cmd, NULL);
-	expect_row(14, T, "     %d  y%71s", 1, "");
-	expect_row(15, T, "     %d  y%71s", 6, "");
-	return rv;
-}
-
-static int
-test_vis(int fd)
-{
-	int y = 1002;
-	(void) fd;
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	check_cmd(T, "tput civis", "*23x80@0,0", ++y);
-	check_cmd(T, "tput cvvis", "*23x80@0,0(%d,%d)", ++y, strlen(T->ps1));
-	return rv;
-}
-
-static int
-test_ech(int fd)
-{
-	(void) fd;
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	check_cmd(T, "printf 012345; tput cub 3; tput ech 1; echo", NULL);
-	expect_row(2, T, "012 45%-74s", "");
-	return rv;
-}
-
-static int
-test_insert(int fd)
-{
-	int y = 1002;
-	(void) fd;
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	check_cmd(T, "printf 0123456; tput cub 3; tput smir; "
-		"echo foo; tput rmir", "*23x80@0,0(%d,6)", y += 2);
-	expect_row(y - 1001 - 1, T, "0123foo456%-70s", "");
-	expect_row(y - 1001, T, "%-80s", T->ps1);
-	return rv;
-}
-
-static int
-test_el(int fd)
-{
-	int y = 1002;
-	(void) fd;
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	check_cmd(T, "printf 01234; tput cub 3; tput el", "*23x80@0,0(%d,%d)",
-		++y, 2 + strlen(T->ps1));
-	expect_row(y - 1001, T, "01%-78s", T->ps1);
-
-	check_cmd(T, "printf 01234; tput cub 3; tput el1; echo",
-		"*23x80@0,0(%d,%d)", y += 2, strlen(T->ps1));
-	expect_row(y - 1001 - 1, T, "   34%75s", "");
-	return rv;
-}
-
-static int
-test_pager(int fd)
-{
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	size_t plen = strlen(T->ps1);;
-	char *lay;
-	fd = fileno(T->fp);
-	char cmd[] = "yes | nl | sed 500q | more\rq";
-
-	rewrite(fd, cmd, sizeof cmd - 1);
-	check_cmd(T, "", "*23x80@0,0(1023,%d)", plen);
-	expect_row(1, T, "     2%-74s", "  y");
-	expect_row(21, T, "    22%-74s", "  y");
-
-	create(T->c, "c");
-	expect_layout(T->c, lay = "*11x80@0,0(1023,%d); 11x80@12,0(1000,0)",
-		plen);
-	check_cmd(T, cmd, lay, plen);
-	expect_row(9, T, "    10%-74s", "  y");
-	return rv;
-}
-
-static int
-test_vpa(int fd)
-{
-	(void) fd;
-	struct test_canvas *T = new_test_canvas(24, 80, NULL);
-	check_cmd(T, "tput vpa 7; tput hpa 18", "*23x80@0,0(%d,%d)",
-		S.history - 23 + 7, 18 + strlen(T->ps1));
-	return rv;
-}
-
-static int
-test_cursor(int fd)
-{
-	int y = 1002;
-	/* many below tests expect ps1 length 6 */
-	struct test_canvas *T = new_test_canvas(24, 80, "uniq> ");
-	(void)fd;
-
-	/* (1) */
-	check_cmd(T, "printf '0123456'; tput cub 4", "*23x80@0,0(%d,9)", ++y);
-	expect_row(y - 1001, T, "012%-77s", T->ps1);
-	check_cmd(T, "tput sc", "*23x80@0,0(%d,6)", ++y);
-	check_cmd(T, "tput rc", "*23x80@0,0(%d,6)", y);
-	y = S.history - 24 + 15;
-	check_cmd(T, "tput cup 15 50;", "*23x80@0,0(%d,56)", ++y);
-	check_cmd(T, "tput clear", "*23x80@0,0(%d,6)", y -= 15);
-	check_cmd(T, "tput ht", "*23x80@0,0(%d,14)", ++y);
-	check_cmd(T, "printf 'a\\tb\\tc\\t'; tput cbt; tput cbt; printf foo",
-		"*23x80@0,0(%d,17)", ++y);
-	expect_row(y - 1001, T, "a       foo%-69s", T->ps1);
-
-	check_cmd(T, "tput cud 6", "*23x80@0,0(%d,6)", y += 1 + 6);
-	check_cmd(T, "printf foobar; tput cub 3; tput dch 1; echo",
-		"*23x80@0,0(%d,6)", y += 2);
-	expect_row(y - 1001 - 1, T, "fooar%75s", " ");
-	expect_row(y - 1001, T, "%-80s", T->ps1);
-
-	check_cmd(T, "printf 012; tput cub 2; tput ich 2; echo",
-		"*23x80@0,0(%d,6)", y += 2);
-	expect_row(y - 1001 - 1, T, "0  12%75s", " ");
-
-	check_cmd(T, "tput cud 6", "*23x80@0,0(%d,6)", y += 1 + 6);
-	check_cmd(T, ":", "*23x80@0,0(%d,6)", ++y);
-	check_cmd(T, ":", "*23x80@0,0(%d,6)", ++y);
-	assert( y == 1023 );
-	check_cmd(T, ":", "*23x80@0,0(%d,6)", y);
-
-	return rv;
-}
-/* (1) I expect the x coordinate of this test to be 6 (the length
-of the prompt, but it consistently comes back 8.  Need to understand
-where the extra 2 characters come from.  This same behavior was
-observed when the prompt was only one character long.
-*/
 
 static int
 test1(int fd)
@@ -537,31 +222,17 @@ describe_layout(char *d, size_t siz, const struct canvas *c, int recurse,
 }
 
 typedef int test(int);
-struct st { char *name; test *f; int main; };
-static int execute_test(struct st *v, const char *argv0);
-#define F(x, y) { .name = #x, .f = (x), .main = y }
+struct st { char *name; test *f; };
+static int execute_test(struct st *v);
+#define F(x) { .name = #x, .f = (x) }
 int
 main(int argc, char *const argv[])
 {
 	int status = 0;
-	const char *argv0 = argv[0];
 	struct st tab[] = {
-		F(test1, 1),
-		F(test_cursor, 0),
-		F(test_vpa, 0),
-		F(test_el, 0),
-		F(test_description, 0),
-		F(test_insert, 0),
-		F(test_vis, 0),
-		F(test_ech, 0),
-		F(test_ich, 0),
-		F(test_scrollback, 0),
-		F(test_nel, 0),
-		F(test_pager, 0),
-		F(test_cols, 0),
-		F(test_csr, 0),
-		F(test_navigate, 1),
-		{ NULL, NULL, 0 }
+		F(test1),
+		F(test_navigate),
+		{ NULL, NULL }
 	}, *v;
 	setenv("SHELL", "/bin/sh", 1);
 	setenv("LINES", "24", 1);
@@ -573,7 +244,7 @@ main(int argc, char *const argv[])
 				;
 		}
 		if( v->f ) {
-			status |= execute_test(v, argv0);
+			status |= execute_test(v);
 		} else {
 			fprintf(stderr, "unknown function: %s\n", name);
 			status = EXIT_FAILURE;
@@ -583,13 +254,13 @@ main(int argc, char *const argv[])
 }
 
 static int
-execute_test(struct st *v, const char *argv0)
+execute_test(struct st *v)
 {
 	char *const args[] = { v->name, v->name, NULL };
 	int fd[2]; /* primary/secondary fd of pty */
 	int status;
 
-	if( v->main && pipe(child_pipe) ) {
+	if( pipe(child_pipe) ) {
 		err(EXIT_FAILURE, "pipe");
 	}
 	if( openpty(fd, fd + 1, NULL, NULL, NULL) ) {
@@ -607,34 +278,24 @@ execute_test(struct st *v, const char *argv0)
 			err(EXIT_FAILURE, "login_tty");
 		}
 		rv = 0;
-		if( v->main ) {
-			struct sigaction sa;
-			memset(&sa, 0, sizeof sa);
-			sa.sa_flags = 0;
-			sigemptyset(&sa.sa_mask);
-			sa.sa_handler = huphandler;
-			sigaction(SIGHUP, &sa, NULL);
-			if( close(child_pipe[0])) {
-				err(EXIT_FAILURE, "close read side");
-			}
-			exit(smtx_main(1, args + 1));
-		} else {
-			if( strcmp(argv0, v->name) ) {
-				execv(argv0, args);
-				perror("execv");
-			}
-			exit(v->f(fd[0]));
+		struct sigaction sa;
+		memset(&sa, 0, sizeof sa);
+		sa.sa_flags = 0;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_handler = huphandler;
+		sigaction(SIGHUP, &sa, NULL);
+		if( close(child_pipe[0])) {
+			err(EXIT_FAILURE, "close read side");
 		}
+		exit(smtx_main(1, args + 1));
 	default:
-		if( v->main ) {
-			if( close(fd[1]) ) {
-				err(EXIT_FAILURE, "close secondary");
-			}
-			if( close(child_pipe[1])) {
-				err(EXIT_FAILURE, "close write side");
-			}
-			rv = v->f(fd[0]);
+		if( close(fd[1]) ) {
+			err(EXIT_FAILURE, "close secondary");
 		}
+		if( close(child_pipe[1])) {
+			err(EXIT_FAILURE, "close write side");
+		}
+		rv = v->f(fd[0]);
 		wait(&status);
 	}
 	if( WIFEXITED(status) && WEXITSTATUS(status) != 0 ) {
