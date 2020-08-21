1e7234c09653f31a6bf7f1d248de4c853a96c9a3
William Pursell <william.r.pursell@gmail.com>
2020-08-21 17:26:37 +0000

    Add smtx-test.h

---
+++ smtx-test.h
@@ -0,0 +1,7 @@
+/* Functions which are exposed to the test suite */
+
+extern unsigned describe_layout(char *, ptrdiff_t, unsigned);
+extern unsigned describe_row(char *, size_t, int);
+extern int smtx_main(int, char *const*);
+
+#define CTL(x) ((x) & 0x1f)
