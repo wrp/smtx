wrp/smtx

commit 8805e2acfba83d77f3e0768f302336a235e7fb2e

Read input from stdscr

smtx-main.c
@@ -990,7 +990,7 @@ main_loop(void)
 			}
 			FD_ZERO(&sfds);
 		}
-		while( (r = wget_wch(S.f->input, &w)) != ERR ) {
+		while( (r = get_wch(&w)) != ERR ) {
 			handlechar(r, w);
 			fixcursor();
 		}
@@ -1062,6 +1062,7 @@ init(void)
 	raw();
 	noecho();
 	nonl();
+	timeout(0);
 	intrflush(NULL, FALSE);
 	start_color();
 	use_default_colors();

commit 618ebed46f6c3c9eef3cb5a8b49f50169c74796e

Use wtimeout vice nodelay
I was experimenting with timeout == 1 to resolve the issue
introduced by 8805e2acfba83d7 (see ISSUES)

smtx-main.c
@@ -127,7 +127,7 @@ resize_pad(WINDOW **p, int h, int w)
 	if( *p ) {
 		err_check(wresize(*p, h, w ) != OK, "Error resizing window");
 	} else if( (*p = newpad(h, w)) != NULL ) {
-		err_check(nodelay(*p, TRUE) != OK, "Error setting delay");
+		wtimeout(*p, 0);
 	}
 }
 