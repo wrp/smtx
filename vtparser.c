9bd4935f64062be34aa014ac844ba52d06284bb9
William Pursell <william.r.pursell@gmail.com>
2020-10-13 13:54:00 +0000

    Static initialization of csi_param state

--- vtparser.c
+++ vtparser.c
@@ -222,6 +222,26 @@ static struct state csi_ignore = {
 	}
 };
 
+static struct state csi_param = {
+	.entry = NULL,
+	.act = {
+		[0]             = {ignore, NULL},
+		[0x01 ... 0x17] = {docontrol, NULL},
+		[0x18]          = {docontrol, &ground},
+		[0x19]          = {docontrol, NULL},
+		[0x1a]          = {docontrol, &ground},
+		[0x1b]          = {ignore, &esc},
+		[0x1c ... 0x1f] = {docontrol, NULL},
+		[0x20 ... 0x2f] = {collect, &csi_intermediate},
+		[0x30 ... 0x39] = {param, NULL},
+		[0x3a]          = {ignore, &csi_ignore},
+		[0x3b]          = {param, NULL},
+		[0x3c ... 0x3f] = {ignore, &csi_ignore},
+		[0x40 ... 0x7e] = {docsi, &ground},
+		[0x7f]          = {ignore, NULL},
+	}
+};
+
 void
 vtonevent(struct vtp *vp, enum vtEvent t, wchar_t w, int cb)
 {
@@ -312,14 +332,6 @@ static void
 init(void)
 {
 	initialized = 1;
-
-	initstate(&csi_param, NULL);
-	init_range(&csi_param, 0x20, 0x2f, collect, &csi_intermediate);
-	init_range(&csi_param, 0x30, 0x39, param, NULL);
-	init_action(&csi_param, 0x3a, ignore, &csi_ignore);
-	init_action(&csi_param, 0x3b, param, NULL);
-	init_range(&csi_param, 0x3c, 0x3f, ignore, &csi_ignore);
-	init_range(&csi_param, 0x40, 0x7e, docsi, &ground);
 
 	initstate(&csi_intermediate, NULL);
 	init_range(&csi_intermediate, 0x20, 0x2f, collect, NULL);
