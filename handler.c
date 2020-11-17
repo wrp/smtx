wrp/smtx

commit 9528cf9afb32f840c3b1f9ca4cf2b1bc140de12d

Make fg/bg an array
It just seems cleaner to write s->c.fgbg[k] than to write
*(k ? &s->c.bg : &s->c.fg)

handler.c
@@ -43,7 +43,7 @@ restore_cursor(struct screen *s)
 	if( s->sc.gc ) {
 		cchar_t b;
 		s->c = s->sc;
-		int pair = alloc_pair(s->c.fg, s->c.bg);
+		int pair = alloc_pair(s->c.fgbg[0], s->c.fgbg[1]);
 		wattr_set(s->win, s->sattr, pair, NULL);
 		setcchar(&b, L" ", A_NORMAL, pair, NULL);
 		wbkgrndset(s->win, &b);
@@ -55,14 +55,14 @@ save_cursor(struct screen *s)
 {
 	short pair;
 	wattr_get(s->win, &s->sattr, &pair, NULL);
-	pair_content(pair, &s->sc.fg, &s->sc.bg);
+	pair_content(pair, s->sc.fgbg, s->sc.fgbg + 1);
 	s->sc = s->c;
 }
 
 static void
 reset_sgr(struct screen *s)
 {
-	pair_content(COLOR_PAIR(0), &s->c.fg, &s->c.bg);
+	pair_content(COLOR_PAIR(0), s->c.fgbg, s->c.fgbg + 1);
 	wcolor_set(s->win, COLOR_PAIR(0), NULL);
 	wattr_set(s->win, A_NORMAL, COLOR_PAIR(0), NULL);
 	wbkgdset(s->win, COLOR_PAIR(0) | ' ');
@@ -188,8 +188,8 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			rewrite(p->fd, "\033[?6c", 5);
 		}
 	Kase ech:
-		setcchar(&b, L" ", A_NORMAL, alloc_pair(s->c.fg, s->c.bg),
-			NULL);
+		setcchar(&b, L" ", A_NORMAL,
+			alloc_pair(s->c.fgbg[0], s->c.fgbg[1]), NULL);
 		for( i = 0; i < p0[1]; i++ ) {
 			mvwadd_wchnstr(win, s->c.y, s->c.x + i, &b, 1);
 		}
@@ -212,7 +212,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 				wmove(win, s->c.y, s->c.x);
 			}
 			setcchar(&b, L" ", A_NORMAL,
-				alloc_pair(s->c.fg, s->c.bg), NULL);
+				alloc_pair(s->c.fgbg[0], s->c.fgbg[1]), NULL);
 			for( i = 0; i <= s->c.x; i++ ) {
 				mvwadd_wchnstr(win, s->c.y, i, &b, 1);
 			}
@@ -328,7 +328,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 		}
 		for( i = 0; i < argc; i++ ) {
 			bool at = argc > i + 2 && argv[i + 1] == 5;
-			int val = argc > i + 2 ? argv[i + 2] : 0, k = 0, a;
+			int val = argc > i + 2 ? argv[i + 2] : 0, k = 1, a;
 			switch( a = argv[i] ) {
 			case  0: reset_sgr(s);
 			Kase  1:
@@ -353,7 +353,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			case 35:
 			case 36:
 			case 37:
-				k = 1; /* Fallthru */
+				k = 0; /* Fallthru */
 			case 40:
 			case 41:
 			case 42:
@@ -362,18 +362,18 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			case 45:
 			case 46:
 			case 47:
-				*(k ? &s->c.fg : &s->c.bg) = colors[a - (k?30:40)];
+				s->c.fgbg[k] = colors[a - ( k ? 40 : 30 )];
 				doc = COLORS >= 8;
 			Kase 38:
 			case 48:
 				if( at ) {
-					*(a == 48 ? &s->c.bg : &s->c.fg) = val;
+					s->c.fgbg[a == 48] = val;
 				}
 				i += 2;
 				doc = COLORS >= 256;
 			Kase 39:
 			case 49:
-				*(a == 49 ? &s->c.bg : &s->c.fg) = -1;
+				s->c.fgbg[a == 49] = -1;
 				doc = true;
 			Kase 90:
 			case 91:
@@ -383,7 +383,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			case 95:
 			case 96:
 			case 97:
-				k = 1; /* Fallthru */
+				k = 0; /* Fallthru */
 			case 100:
 			case 101:
 			case 102:
@@ -392,7 +392,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			case 105:
 			case 106:
 			case 107:
-				*(k ? &s->c.fg : &s->c.bg) = colors[a - (k?90:100)];
+				s->c.fgbg[k] = colors[a - ( k ? 90 : 100 )];
 				doc = COLORS >= 16;
 			#if HAVE_A_ITALIC
 			Kase  3:  wattron(win,  A_ITALIC);
@@ -401,7 +401,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			}
 		}
 		if( doc ) {
-			int pair = alloc_pair(s->c.fg, s->c.bg);
+			int pair = alloc_pair(s->c.fgbg[0], s->c.fgbg[1]);
 			wcolor_set(win, pair, NULL);
 			setcchar(&b, L" ", A_NORMAL, pair, NULL);
 			wbkgrndset(win, &b);

smtx.h
@@ -67,7 +67,7 @@ struct screen {
 	int delta; /* Number of lines written by a vtwrite */
 	struct { int top; int bot; } scroll;
 	/* cursor/save cursor */
-	struct { int y, x, xenl; short fg, bg; wchar_t *gc, *gs; } sc, c;
+	struct { int y, x, xenl; short fgbg[2]; wchar_t *gc, *gs; } sc, c;
 	bool insert;
 	wchar_t repc; /* character to be repeated */
 	int decawm; /* Wrap-around mode */

commit 09c87ee63973bde12940f91c0f4210d267c78c9a

Fix bg indexing error
This error was introduced in 9528cf9afb32f840c3b1f9ca4cf2b1bc140de12d, and
I am disturbed that it was not noticed by the test suite.  We need to figure
out a way to properly test colors > 16.

handler.c
@@ -372,7 +372,7 @@ tput(struct pty *p, wchar_t w, wchar_t iw, int argc, void *arg, int handler)
 			case 105:
 			case 106:
 			case 107:
-				s->c.color[k] = colors[a - ( k ? 90 : 100 )];
+				s->c.color[k] = colors[a - ( k ? 100 : 90 )];
 				doc = COLORS >= 16;
 			}
 		}